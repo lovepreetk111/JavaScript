0994d99cd3e9daf365838e6a8081f147f1bdce3a
klovepreet <86563844+klovepreet@users.noreply.github.com>
2022-05-13 16:07:45 +0530

    Create style.js

---
+++ Login Page/style.js
@@ -0,0 +1,12 @@
+const userName = "Lovepreet";
+const Password = "1234567";
+
+function button() {
+if(document.getElementById('usName').value === userName & document.getElementById('Pass').value === Password){
+    alert('Login Successful');
+}
+else{
+        alert('Login Failed');
+    }
+}
+/* button() */
